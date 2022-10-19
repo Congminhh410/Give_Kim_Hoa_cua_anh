7fc9e5bb6a0351df7590c7dd8d35f649c766279b
Congminhh410 <86812135+Congminhh410@users.noreply.github.com>
2022-10-19 23:58:19 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,18 @@
 const CONFIG = {
-    introTitle: 'Em à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
+    introTitle: 'Hê sờ nhô cô gái xinh đẹp nhất hôm nay!',
+    introDesc: `Hôm nay là ngày 20-10.
+    Là 1 ngày khá đặc biệt của cô gái đang đọc những dòng này từ anh
+    Cô gái hôm nay phải thật vui vẻ và hạnh phúc nghe chưa`,
+    btnIntro: 'Dạ, ô sờ kê ạ <3',
+    title: 'Hôm nay là 20-10 nhỉ 😙',
+    desc: 'Anh chúc cô gái xinh đẹp thật vui vẻ và hạnh phúc, luôn phải vui tươi và cười nhiều hơn nữa. Và 1 điều nữa anh muốn nhắn gửi đến cô gái là trong tim cô gái luôn phải chừa 1 chỗ cho anh đó nghe chưa ! Không trả lời tức là đồng ý đó >.<',
+    btnYes: 'Vẫn luôn có 1 chỗ duy nhất cho anh <33',
     btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Tại anh là cả thế giới của em <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    question:'Thứ em thực sự thích vào ngày 20-10 này là gì vậy cô bé ?',
+    btnReply: 'Gửi cho anh nào cô bé <3',
+    reply: 'Không chỉ 20-10 mà cả tương lai em chỉ muốn có anh bên cạnh thôi <33333333',
+    mess: 'Anh biết mà 🥰. Anh cũng đang đợi điều đó đến với anh nè 😘😘',
+    messDesc: 'Đùa thôi, anh chỉ muốn 20-10 này cho cô gái 1 chút vui vẻ và đặc biệt thôi. Nhưng anh cũng mong điều đó sớm sẽ thành sự thật. Cuối cùng tới đây và nói gì đó với anh đi nào babe <3',
+    btnAccept: 'Hòm thư góp ý nè cô bé. Đến đây và góp ý gì đó cho anh nào <3',
     messLink: 'http://m.me/congminhh410' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
