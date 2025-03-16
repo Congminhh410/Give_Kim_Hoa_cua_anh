69564d09547b352bbe0f5b340183a1ec19b093c5
Congminhh410 <86812135+Congminhh410@users.noreply.github.com>
2025-03-16 21:31:07 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,15 +4,15 @@ const CONFIG = {
     Tháng 3 là tháng rất đặc biệt em đến với thế giới này và bây giờ em là của anh
     Và sau này anh cũng mong nó là 1 ngày đặc biệt đối với anh nữa`,
     btnIntro: 'Tiếp theo 👉',
-    title: 'Chúc mừng sinh nhật em, cũng chúc mừng tình yêu của chúng ta 😙',
-    desc: 'Em bảo chưa thực sự là của tôi ư. Hôm nay tại đây tôi muốn biết em sẽ đồng ý làm người con gái của anh chứ 😙 ',
+    title: 'Em bảo chưa thực sự là của tôi ư. Hôm nay tại đây tôi muốn biết em sẽ đồng ý làm người con gái của anh chứ 😙',
+    desc: 'Em hãy chọn 1 trong 2 đáp án nhé 😍',
     btnYes: 'Chắc chắn là em sẽ đồng ý không điều kiện rùi 😘',
     btnNo: 'Mơ đi, em phải suy nghĩ đã 😮‍💨',
     question:'Anh biết là em sẽ chọn đáp án này mà 🥰. Love you 😘😘. Hãy viết 1 điều mà em muốn gửi tới anh nhé. Điều này sẽ được giữ bí mật 😮‍💨',
     btnReply: 'Gửi vào hòm thư bí mật <3',
     reply: 'Thực ra gặp được anh là em thấy mình thật sự rất may mắn rồi <3333333333333333',
     mess: 'Thật may mắn là trong 7 tỷ người và chúng ta gặp được nhau 💕💕💕',
-    messDesc: 'Anh mong từ giờ trở về sau, chúng ta sẽ mãi bên nhau vậy nhé <3',
+    messDesc: 'Anh mong từ giờ trở về sau, chúng ta sẽ mãi bên nhau vậy nhé. Chúc mừng sinh nhật em 21/3 <333',
     btnAccept: 'Dạ vânggggggg <3',
     messLink: 'https://www.messenger.com/t/congminhh410' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
